a5ec2f0ac706522fc6e159ecb147c85a31a92eeb
edward4secret@hotmail.com <edward4secret@hotmail.com>
2021-09-23 13:02:09 -0500

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import  Navbar  from './Components/Navbar.js';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,1 +1,39 @@
-import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarToggler"
+        aria-controls="navbarToggler"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <a className="navbar-brand" href="/">
+        Navbar
+      </a>
+
+      <div className="collapse navbar-collapse" id="navbarToggler">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Link
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
