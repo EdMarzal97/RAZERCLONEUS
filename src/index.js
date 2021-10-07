9fb893669488750148b96d730d9055e308995bd0
edward4secret@hotmail.com <edward4secret@hotmail.com>
2021-10-07 14:17:17 -0500

    navbar update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import "bootstrap/dist/js/bootstrap.js";
 
 ReactDOM.render(
   <React.StrictMode>
